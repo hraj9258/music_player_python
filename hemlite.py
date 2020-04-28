a68a31859345577596ad29022246e2374d5dbfac
Himanshu Raj <himanshurajkush@gmail.com>
2020-04-28 22:42:45 +0530

    initial

---
+++ hemlite.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("300x300")
+root.title("Melody")
+#root.iconbitmap(r"melody.ico")
+text=Label(root,text="Lets! Make some noise.")
+text.pack()
+root.mainloop()
