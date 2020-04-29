1a8edceddef79378b4766e8f9ed6d56d88db2550
Himanshu Raj <himanshurajkush@gmail.com>
2020-04-29 20:40:18 +0530

    Added Button

--- hemlite.py
+++ hemlite.py
@@ -3,8 +3,16 @@ from tkinter import *
 root = Tk()
 root.geometry("300x300")
 root.title("Melody")
-#root.iconbitmap(r"melody.ico")
+root.iconbitmap(r"icon.ico")
+
 text=Label(root,text="Lets! Make some noise.")
-print("hello")
 text.pack()
+
+def play_btn():
+    print("Play button works very well!")
+
+photo=PhotoImage(file="play_2.png")
+btn=Button(root, image=photo, command=play_btn)
+btn.pack()
+
 root.mainloop()
